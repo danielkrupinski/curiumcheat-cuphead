89a36a6826b2459eff8adc7a4aa9a6a6e6f7c947
Daniel Krupiński <34515229+danielkrupinski@users.noreply.github.com>
2017-12-17 14:06:09 +0100

    Optimization and bug fix
    Code simplification. Change pPlayer variable type from DWORD to size_t.

--- curiumcheat-cuphead.cpp
+++ curiumcheat-cuphead.cpp
@@ -1,44 +1,33 @@
 #include <iostream>
 #include <Windows.h>
 
-using namespace std;
-
 int main()
 {
 	HANDLE handle = NULL;
 	
 	while (handle == NULL)
 	{
-		printf("Searching for game process");
-		Sleep(900);
-		printf(".");
-		Sleep(900);
-		printf(".");
-		Sleep(900);
-		printf(".");
-		Sleep(900);
 		HWND hwnd = FindWindowA(0, ("Cuphead"));
 		DWORD processID;
 		GetWindowThreadProcessId(hwnd, &processID);
 		handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
-		system("cls");
+		Sleep(1);
 	}
 
-	system("cls");
 	printf("Process found!");
 
-	DWORD pPlayer = 0;
-	BYTE invictible = 1;
+	BYTE invincibility = 1;
+	size_t pPlayer = 0;
 
 	for (;;)
 	{
-		ReadProcessMemory(handle, (LPVOID)(0x1020A13C), &pPlayer, sizeof(pPlayer), 0);
-		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x740), &pPlayer, sizeof(pPlayer), 0);
-		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x34), &pPlayer, sizeof(pPlayer), 0);
-		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x8), &pPlayer, sizeof(pPlayer), 0);
-		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x18), &pPlayer, sizeof(pPlayer), 0);
-		WriteProcessMemory(handle, (LPVOID)(pPlayer + 0x6C), &invictible, sizeof(invictible), 0);
-		Sleep(5000);
+		ReadProcessMemory(handle, (LPVOID)(0x1020A13C), &pPlayer, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x740), &pPlayer, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x34), &pPlayer, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x8), &pPlayer, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x18), &pPlayer, sizeof(int), 0);
+		WriteProcessMemory(handle, (LPVOID)(pPlayer + 0x6C), &invincibility, sizeof(invincibility), 0);
+		Sleep(1);
 	}
 	return 0;
 }
