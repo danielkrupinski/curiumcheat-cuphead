fb4a8edd239851c05424a8b08376dc85f339922f
Daniel Krupiński <34515229+danielkrupinski@users.noreply.github.com>
2017-12-17 13:35:52 +0100

    Add invincibility
    Invincibility replaces hp cheat

--- curiumcheat-cuphead.cpp
+++ curiumcheat-cuphead.cpp
@@ -27,41 +27,18 @@ int main()
 	system("cls");
 	printf("Process found!");
 
-	int pPlayer = 0;
-
-	ReadProcessMemory(handle, (LPVOID)(0x1020A13C), &pPlayer, sizeof(pPlayer), 0);
-	ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x740), &pPlayer, sizeof(pPlayer), 0);
-	ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x34), &pPlayer, sizeof(pPlayer), 0);
-	ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x8), &pPlayer, sizeof(pPlayer), 0);
-	ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x18), &pPlayer, sizeof(pPlayer), 0);
-
-	system("cls");
-	printf("Select max health you want: ");
-	int NewHealth = 0;
-	cin >> NewHealth;
-	WriteProcessMemory(handle, (LPVOID)(pPlayer + 0x5C), &NewHealth, sizeof(NewHealth), 0);
-
-	int counter = 0;
+	DWORD pPlayer = 0;
+	BYTE invictible = 1;
 
 	for (;;)
 	{
-		counter++;
-		int pPlayer1 = 0;
-		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x60), &pPlayer1, sizeof(pPlayer1), 0);
-		if (pPlayer1!=NewHealth)
-		{
-			WriteProcessMemory(handle, (LPVOID)(pPlayer + 0x60), &NewHealth, sizeof(NewHealth), 0);
-		}
-		Sleep(1000);
-		if (counter==5)
-		{
-			ReadProcessMemory(handle, (LPVOID)(0x1020A13C), &pPlayer, sizeof(pPlayer), 0);
-			ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x740), &pPlayer, sizeof(pPlayer), 0);
-			ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x34), &pPlayer, sizeof(pPlayer), 0);
-			ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x8), &pPlayer, sizeof(pPlayer), 0);
-			ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x18), &pPlayer, sizeof(pPlayer), 0);
-			counter = 0;
-		}
+		ReadProcessMemory(handle, (LPVOID)(0x1020A13C), &pPlayer, sizeof(pPlayer), 0);
+		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x740), &pPlayer, sizeof(pPlayer), 0);
+		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x34), &pPlayer, sizeof(pPlayer), 0);
+		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x8), &pPlayer, sizeof(pPlayer), 0);
+		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x18), &pPlayer, sizeof(pPlayer), 0);
+		WriteProcessMemory(handle, (LPVOID)(pPlayer + 0x6C), &invictible, sizeof(invictible), 0);
+		Sleep(5000);
 	}
 	return 0;
 }
