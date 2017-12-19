6cab61599defe13f4575e9220d0627d747e5de25
danielkrupinski <danielkrupinski@outlook.com>
2017-12-19 17:54:24 +0100

    Add experimental coins cheat

--- curiumcheat-cuphead.cpp
+++ curiumcheat-cuphead.cpp
@@ -27,24 +27,37 @@ int main()
 
 	BYTE invincibility = 1;
 	float supermeter = 50;
-	size_t pPlayer = 0;
+	//int coins = 60;
+	size_t player = 0;
+	//size_t money = 0;
 
 	for (;;)
 	{
-		ReadProcessMemory(handle, (LPVOID)(0x1020A13C), &pPlayer, sizeof(int), 0);
-		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x740), &pPlayer, sizeof(int), 0);
-		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x34), &pPlayer, sizeof(int), 0);
-		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x8), &pPlayer, sizeof(int), 0);
-		ReadProcessMemory(handle, (LPVOID)(pPlayer + 0x18), &pPlayer, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(0x1020A13C), &player, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(player + 0x740), &player, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(player + 0x34), &player, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(player + 0x8), &player, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(player + 0x18), &player, sizeof(int), 0);
+
+		// experimental coins cheat (buggy)
+
+		/*
+		ReadProcessMemory(handle, (LPVOID)(0x101F40AC), &money, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(money + 0xCC), &money, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(money + 0x320), &money, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(money + 0x658), &money, sizeof(int), 0);
+		ReadProcessMemory(handle, (LPVOID)(money + 0x12C), &money, sizeof(int), 0);
+		WriteProcessMemory(handle, (LPVOID)(money + 0xB4), &coins, sizeof(coins), 0);
+		*/
 
 		switch (mode)
 		{
 		case 1:
-			WriteProcessMemory(handle, (LPVOID)(pPlayer + 0x84), &invincibility, sizeof(invincibility), 0);
-			WriteProcessMemory(handle, (LPVOID)(pPlayer + 0x68), &supermeter, sizeof(supermeter), 0);
+			WriteProcessMemory(handle, (LPVOID)(player + 0x84), &invincibility, sizeof(invincibility), 0);
+			WriteProcessMemory(handle, (LPVOID)(player + 0x68), &supermeter, sizeof(supermeter), 0);
 			break;
 		case 2:
-			WriteProcessMemory(handle, (LPVOID)(pPlayer + 0x6C), &invincibility, sizeof(invincibility), 0);
+			WriteProcessMemory(handle, (LPVOID)(player + 0x6C), &invincibility, sizeof(invincibility), 0);
 			break;
 		default:
 			return 0;
