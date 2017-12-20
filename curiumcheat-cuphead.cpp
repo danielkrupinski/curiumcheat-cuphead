4d74654bb7af8df44fbd4de6b0ebe1cac2c58697
danielkrupinski <danielkrupinski@outlook.com>
2017-12-20 15:19:22 +0100

    Add comments

--- curiumcheat-cuphead.cpp
+++ curiumcheat-cuphead.cpp
@@ -32,20 +32,21 @@ int main()
 
 	for (;;)
 	{
-		ReadProcessMemory(handle, (LPVOID)(0x1020A13C), &player, sizeof(int), 0);
-		ReadProcessMemory(handle, (LPVOID)(player + 0x740), &player, sizeof(int), 0);
-		ReadProcessMemory(handle, (LPVOID)(player + 0x34), &player, sizeof(int), 0);
-		ReadProcessMemory(handle, (LPVOID)(player + 0x8), &player, sizeof(int), 0);
-		ReadProcessMemory(handle, (LPVOID)(player + 0x18), &player, sizeof(int), 0);
+		/* Get player base address */
+		ReadProcessMemory(handle, (LPVOID)(0x1020A13C), &player, sizeof(int), 0); // mono.dll(0x10000000)+0x20A13C
+		ReadProcessMemory(handle, (LPVOID)(player + 0x740), &player, sizeof(int), 0); // 0x740 - first offset
+		ReadProcessMemory(handle, (LPVOID)(player + 0x34), &player, sizeof(int), 0); // 0x34 - second offset
+		ReadProcessMemory(handle, (LPVOID)(player + 0x8), &player, sizeof(int), 0); // 0x8 - third offset
+		ReadProcessMemory(handle, (LPVOID)(player + 0x18), &player, sizeof(int), 0); // 0x18 - fourth offset
 
 		switch (mode)
 		{
 		case 1:
-			WriteProcessMemory(handle, (LPVOID)(player + 0x84), &invincibility, sizeof(invincibility), 0);
-			WriteProcessMemory(handle, (LPVOID)(player + 0x68), &supermeter, sizeof(supermeter), 0);
+			WriteProcessMemory(handle, (LPVOID)(player + 0x84), &invincibility, sizeof(invincibility), 0); // invincibility - player base adress + 0x84
+			WriteProcessMemory(handle, (LPVOID)(player + 0x68), &supermeter, sizeof(supermeter), 0); // super meter - player base adress + 0x68
 			break;
 		case 2:
-			WriteProcessMemory(handle, (LPVOID)(player + 0x6C), &invincibility, sizeof(invincibility), 0);
+			WriteProcessMemory(handle, (LPVOID)(player + 0x6C), &invincibility, sizeof(invincibility), 0); // full invincibility - player base adress + 0x6C
 			break;
 		default:
 			return 0;
